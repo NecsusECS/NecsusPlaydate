4f8915662102f35a457bf5fff1e974de0ff69fd0
Nycto <James@RoundEights.com>
2025-04-22 18:02:35 -0700

    isqrt

--- tests/t_util.nim
+++ tests/t_util.nim
@@ -7,3 +7,11 @@ suite "Utilities":
     unroll i, value, inputs:
       output.add((i, value))
     check(output == @[(0'i32, "a"), (1'i32, "b"), (2'i32, "c")])
+
+  test "Integer square roots":
+    check(isqrt(0) == 0)
+    check(isqrt(1) == 1)
+    check(isqrt(4) == 2)
+    check(isqrt(8) == 2)
+    check(isqrt(9) == 3)
+    check(isqrt(10) == 3)
