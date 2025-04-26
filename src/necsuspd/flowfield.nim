10df46885c1b106a6c8c1d6b222e2baf558167d5
Nycto <James@RoundEights.com>
2025-04-25 18:43:42 -0700

    Flow field generation

---
+++ src/necsuspd/flowfield.nim
@@ -0,0 +1,40 @@
+import fixedpoint, vmath
+
+type
+  FlowFieldInput* =
+    concept i
+        ## An input from which a flow field can be generated.
+        i.contains(IVec2) is bool
+        i.cost(IVec2) is SomeNumber
+
+  FlowField*[W, H: static int32] = array[W, array[H, FPVec2]]
+
+export fixedpoint
+
+iterator grid(width, height: static int32): IVec2 =
+  for y in 0'i32 ..< height:
+    for x in 0'i32 ..< width:
+      yield ivec2(x, y)
+
+iterator neighbors(
+    node: IVec2, width, height: static int32
+): tuple[absolute: IVec2, rel: FPVec2] =
+  for dy in -1'i32 .. 1'i32:
+    for dx in -1'i32 .. 1'i32:
+      let nx = node.x + dx
+      let ny = node.y + dy
+      if nx in 0 ..< width and ny in 0 ..< height:
+        yield (ivec2(nx, ny), fpvec2(dx, dy))
+
+proc computeFlowField*[W, H: static int32](map: FlowFieldInput): FlowField[W, H] =
+  ## Compute a flow field of a given size
+  for node in grid(W, H):
+    var total = fpvec2(0, 0)
+    let nodeCost = map.cost(node)
+    for (neighbor, rel) in neighbors(node, W, H):
+      if map.contains(neighbor):
+        let delta = nodeCost - map.cost(neighbor)
+        if delta > 0:
+          total += rel
+
+    result[node.y][node.x] = if total == fpvec2(0, 0): total else: total.normalize
---
+++ tests/t_flowfield.nim
@@ -0,0 +1,46 @@
+import std/[unittest, strutils], necsuspd/flowfield, vmath
+
+type Map[N] = array[N, array[N, int]]
+
+proc `$`(field: FlowField): string =
+  result = ""
+  for row in field:
+    for elem in row:
+      result.add "("
+      result.add $elem.x
+      result.add ", "
+      result.add $elem.y
+      result.add ")"
+    result.add("\n")
+  result.add("\n")
+
+proc contains(map: Map, point: IVec2): bool =
+  point.x >= 0 and point.x < len(map) and point.y >= 0 and point.y < len(map[point.x])
+
+proc cost(map: Map, point: IVec2): int =
+  if contains(map, point):
+    map[point.x][point.y]
+  else:
+    0
+
+suite "FlowField":
+  test "Generate a flow field":
+    #!fmt: off
+    let map: Map[3] = [
+      [4, 3, 2],
+      [3, 2, 1],
+      [2, 1, 0]
+    ]
+    #!fmt: on
+
+    let flowfield = computeFlowField[3, 3](map)
+
+    check(
+      $flowfield ==
+        """
+        (0.703125, 0.703125)(0.703125, 0.703125)(0.0, 1.0)
+        (0.703125, 0.703125)(0.703125, 0.703125)(0.0, 1.0)
+        (1.0, 0.0)(1.0, 0.0)(0.0, 0.0)
+
+        """.dedent
+    )
