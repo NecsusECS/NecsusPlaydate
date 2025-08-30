821a4fcf02e014bf46c19dde6575f7ca96d63e37
Nycto <James@RoundEights.com>
2025-08-29 18:47:43 -0700

    Add DebugInput

--- src/necsuspd/debug.nim
+++ src/necsuspd/debug.nim
@@ -1,6 +1,9 @@
-import macros
+import macros, necsus, playdate/api, util
 
-type DebugKey* = char
+type
+  DebugKey* = char ## A single key pressed by the user.
+
+  DebugInput* = string ## A message received from the debug input handler.
 
 macro debugSys*(system: typed): untyped =
   ## Pragma for a system that only needs to operate when debug mode is enabled
@@ -11,3 +14,12 @@ macro debugSys*(system: typed): untyped =
     quote:
       proc `name`*() =
         discard
+
+proc debugInputHandler*(messages: Outbox[DebugInput]) {.startupSys.} =
+  ## Attaches a handler for debug input messages.
+  when not defined(danger):
+    playdate.system.setSerialMessageCallback(
+      proc(msg: string) =
+        log "Debug message received: ", msg
+        messages(msg)
+    )
