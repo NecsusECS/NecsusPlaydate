a8bc65c9a205e3754044577ae34020ea1c61bd9b
Nycto <James@RoundEights.com>
2025-09-06 19:31:12 -0700

    Add buildHeldButtonRepeater

--- src/necsuspd/inputs.nim
+++ src/necsuspd/inputs.nim
@@ -1,4 +1,4 @@
-import vmath
+import vmath, necsus
 
 when not defined(unittests):
   import playdate/api
@@ -28,12 +28,39 @@ type
 
 proc asVector*(button: PDButton): IVec2 =
   ## Returns a button push as a vector
-  case button:
-  of kButtonLeft: ivec2(-1, 0)
-  of kButtonRight: ivec2(1, 0)
-  of kButtonUp: ivec2(0, -1)
-  of kButtonDown: ivec2(0, 1)
-  of kButtonB, kButtonA: ivec2(0, 0)
+  case button
+  of kButtonLeft:
+    ivec2(-1, 0)
+  of kButtonRight:
+    ivec2(1, 0)
+  of kButtonUp:
+    ivec2(0, -1)
+  of kButtonDown:
+    ivec2(0, 1)
+  of kButtonB, kButtonA:
+    ivec2(0, 0)
+
+const dpadButtons* = {kButtonUp, kButtonDown, kButtonLeft, kButtonRight}
+
+template buildHeldButtonRepeater*(
+    name: untyped, forButtons: set[PDButton], activeStates: typed
+) =
+  ## Builds a system that simulates button presses when a button is held down
+  proc `name`(
+      held: Shared[ButtonsHeld],
+      trigger: Outbox[ButtonPushed],
+      time: TimeElapsed,
+      nextTriggerTime: Local[float32],
+  ) {.active(activeStates).} =
+    let buttons = held.get() * forButtons
+    if buttons.card == 0:
+      nextTriggerTime.clear
+    elif nextTriggerTime.isEmpty:
+      nextTriggerTime := time() + 0.2
+    elif time() >= nextTriggerTime.get():
+      nextTriggerTime := time() + 0.06
+      for button in buttons:
+        trigger(button)
 
 when not defined(unittests):
   import necsus
