256a6a440494048e43572206d28df339ca8b20ef
Nycto <James@RoundEights.com>
2025-09-09 08:13:49 -0700

    Add singleton lib

---
+++ src/necsuspd/singleton.nim
@@ -0,0 +1,22 @@
+import necsus, loading, visibleState, sprite
+
+template createsSingletonSpawner*(
+    name: untyped,
+    entityTag: typed,
+    assetBagType, animationSheetId: typedesc,
+    loadingState, zIndex: enum,
+    initialAnimation: AnimationDef,
+    visibility: VisibleState,
+) =
+  ## Creates a system for spawning a singleton entity
+  proc `name`*(
+      task: Bundle[LoadTasks],
+      assets: Shared[assetBagType],
+      create: Spawn[
+        (typeof(entityTag), Animation[animationSheetId], Positioned, VisibleState)
+      ],
+  ) {.active(loadingState).} =
+    task.execTask($typeof(entityTag), typeof(entityTag), 0):
+      let sheet = assets.newSheet(initialAnimation, zIndex)
+      sheet.visible = false
+      set(create, (entityTag, sheet, positioned(0, 0), visibility))
