5ed7f475ad47d30d99728ee6f0f3b7d4f32dfd17
Nycto <James@RoundEights.com>
2025-10-04 16:18:03 -0700

    Fix default exports file

--- src/necsuspd.nim
+++ src/necsuspd.nim
@@ -1,17 +1,60 @@
 import
   necsuspd/[
-    alignment, aseprite, assetBag, boxelem, carousel, circle, crankAlert, crankData,
-    debounce, debug, draw, easing, fade, findDir, init, inputs, layout, loading, moveTo,
-    multistep, obfuscate, particles, percent, pool, positioned, rand, shake, sprite,
-    textwrap, time, timer, treeSeq, triggerBox, util, value, vec_tools, viewport,
-    visibleState,
+    achievements, alignment, anchor, aseprite, assetBag, autoalign, awards, boxelem, c,
+    carousel, circle, crankAlert, crankData, cursor, data_bundle, debounce, debug,
+    djikstra, draw, easing, fade, files, findDir, flowfield, fpvec, init, inputs,
+    layout, loading, moveTo, multistep, obfuscate, particles, percent, pool, positioned,
+    rand, shake, singleton, sprite, textwrap, time, timer, treeSeq, triggerBox, types,
+    util, vec_tools, viewport, visibleState,
   ]
 
-export
-  necsuspd/[
-    alignment, aseprite, assetBag, boxelem, carousel, circle, crankAlert, crankData,
-    debounce, debug, draw, easing, fade, findDir, init, inputs, layout, loading, moveTo,
-    multistep, obfuscate, particles, percent, pool, positioned, rand, shake, sprite,
-    textwrap, time, timer, treeSeq, triggerBox, util, value, vec_tools, viewport,
-    visibleState,
-  ]
+export achievements
+export alignment
+export anchor
+export aseprite
+export assetBag
+export autoalign
+export awards
+export boxelem
+export c
+export carousel
+export circle
+export crankAlert
+export crankData
+export cursor
+export data_bundle
+export debounce
+export debug
+export djikstra
+export draw
+export easing
+export fade
+export files
+export findDir
+export flowfield
+export fpvec
+export init
+export inputs
+export layout
+export loading
+export moveTo
+export multistep
+export obfuscate
+export particles
+export percent
+export pool
+export positioned
+export rand
+export shake
+export singleton
+export sprite
+export textwrap
+export time
+export timer
+export treeSeq
+export triggerBox
+export types
+export util
+export vec_tools
+export viewport
+export visibleState
