91c929279ca45eb5a32e4e2a6d612991804cd97f
Nycto <James@RoundEights.com>
2026-03-05 18:01:02 -0800

    Add generic to cursor

--- src/necsuspd/cursor.nim
+++ src/necsuspd/cursor.nim
@@ -1,4 +1,5 @@
-import necsus, positioned, inputs, util, fpvec, std/options, vmath, fungus, findDir, sprite
+import
+  necsus, positioned, inputs, util, fpvec, std/options, vmath, fungus, findDir, sprite
 
 adtEnum(Selected):
   NoSelection
@@ -11,12 +12,12 @@ type
   Selectable* {.accessory.} = object
     ## A component that marks entities that are selectable
 
-  CursorControlDirs = object ## A component that controls the cursor
-    find: FullQuery[(Selectable, Positioned, Option[Sprite], Option[Animation])]
+  CursorControlDirs[T] = object ## A component that controls the cursor
+    find: FullQuery[(T, Selectable, Positioned, Option[Sprite], Option[Animation])]
     notify: Outbox[Selected]
     selected: Local[EntitySelected]
 
-  CursorControl* = Bundle[CursorControlDirs]
+  CursorControl*[T] = Bundle[CursorControlDirs[T]]
 
 proc entityId*(selected: Selected): Option[EntityId] =
   ## Returns the entity ID of the selected entity, or None if no entity is selected.
@@ -48,33 +49,31 @@ proc size(sprite: Option[Sprite], anim: Option[Animation]): FPVec2 =
     return fpvec2(it.width, it.height)
   return fpvec2(0, 0)
 
-proc centroid[T](entity: (T, Positioned, Option[Sprite], Option[Animation])): FPVec2 =
-  let (_, pos, sprite, anim) = entity
+proc centroid[A, B](
+    entity: (A, B, Positioned, Option[Sprite], Option[Animation])
+): FPVec2 =
+  let (_, _, pos, sprite, anim) = entity
   return pos.toFPVec2 + (size(sprite, anim) / fpvec2(2, 2))
 
-proc isVisible[A, B](entity: (A, B, Option[Sprite], Option[Animation])): bool =
-  let (_, _, sprite, anim) = entity
-  return sprite.mapIt(it.visible).fallback(anim.mapIt(it.visible)).get(true)
-
-iterator eligible(bundle: CursorControl): (FPVec2, EntityId) =
+iterator eligible[T](bundle: CursorControl[T]): (FPVec2, EntityId) =
   ## An iterator that returns the entities available when choosing a new cursor target
   for eid, entity in bundle.find:
     if bundle.selected.isEmpty or bundle.selected.get().entityId != eid:
-      if entity.isVisible:
-        yield (entity.centroid, eid)
+      yield (entity.centroid, eid)
 
-proc init*(control: CursorControl, startPos: FPVec2 = fpvec2(0, 0)) =
-  ## Initializes the cursor position to the selectable element nearest the given position
+proc init*[T](control: CursorControl[T], startPos: FPVec2 = fpvec2(0, 0)) =
+  ## Initializes the cursor position to the selectable element nearest the given position.
   var nearest: Selected = NoSelection.init().Selected
   var dist: FPInt = high(FPInt)
-  for (coord, eid) in control.eligible():
+  for eid, entity in control.find:
+    let coord = entity.centroid
     let thisDist = distSq(startPos, coord)
     if thisDist < dist:
       nearest = EntitySelected.init(eid, coord)
       dist = thisDist
   control.select(nearest)
 
-proc update*(control: CursorControl, dir: FindDir) =
+proc update*[T](control: CursorControl[T], dir: FindDir) =
   ## Updates the cursor's position in the direction of the given vector
 
   if control.selected.isEmpty:
@@ -85,18 +84,20 @@ proc update*(control: CursorControl, dir: FindDir) =
   let current = control.selected.get().position.toFPVec2
   log "Updating cursor for: ", dir, " with current position: ", current
 
-  for (newPosition, newEntityId) in findDir[EntityId](eligible(control), dir, current):
+  let dir = findDir[EntityId](eligible[T](control), dir, current)
+  if dir.isSome:
+    let (newPosition, newEntityId) = dir.get()
     control.select(EntitySelected.init(newEntityId, newPosition).Selected)
 
-proc update*(control: CursorControl, button: PDButton) =
+proc update*[T](control: CursorControl[T], button: PDButton) =
   ## Updates the cursor's position and visibility based on a button event
   let found = button.asFindDir()
   if found.isSome:
     control.update(found.get())
 
-template buildCursorUpdator*(name: untyped, activeStates: set) =
+template buildCursorUpdator*(name, typ: untyped, activeStates: set) =
   ## Creates a system that updates the cursor's position
   proc `name`(
-      button: ButtonPushed, control: CursorControl
+      button: ButtonPushed, control: CursorControl[typ]
   ) {.active(activeStates), eventSys.} =
     control.update(button)
--- tests/t_cursor.nim
+++ tests/t_cursor.nim
@@ -3,26 +3,27 @@ import
   necsus,
   necsuspd/[cursor, positioned, fpvec, sprite],
   vmath,
-  helpers,
-  necsuspd/stubs/graphics
+  helpers
+
+type MyCursor = object
 
 proc createStdCursorElems(create: auto): auto =
   return [
-    create.with(positioned(50, 60), Selectable()),
-    create.with(positioned(200, 60), Selectable()),
-    create.with(positioned(50, 145), Selectable()),
-    create.with(positioned(79, 145), Selectable()),
-    create.with(positioned(108, 145), Selectable()),
-    create.with(positioned(137, 145), Selectable()),
-    create.with(positioned(42, 190), Selectable()),
+    create.with(MyCursor(), positioned(50, 60), Selectable()),
+    create.with(MyCursor(), positioned(200, 60), Selectable()),
+    create.with(MyCursor(), positioned(50, 145), Selectable()),
+    create.with(MyCursor(), positioned(79, 145), Selectable()),
+    create.with(MyCursor(), positioned(108, 145), Selectable()),
+    create.with(MyCursor(), positioned(137, 145), Selectable()),
+    create.with(MyCursor(), positioned(42, 190), Selectable()),
   ]
 
 template runTest(name: string, body: untyped) =
   runSystemOnce do(
-    create {.inject.}: FullSpawn[(Positioned, Selectable)],
-    createWithAnim {.inject.}: FullSpawn[(Positioned, Selectable, Animation)],
-    createWithSprite {.inject.}: FullSpawn[(Positioned, Selectable, Sprite)],
-    cursor {.inject.}: CursorControl
+    create {.inject.}: FullSpawn[(MyCursor, Positioned, Selectable)],
+    createWithAnim {.inject.}: FullSpawn[(MyCursor, Positioned, Selectable, Animation)],
+    createWithSprite {.inject.}: FullSpawn[(MyCursor, Positioned, Selectable, Sprite)],
+    cursor {.inject.}: CursorControl[MyCursor]
   ) -> void:
     test name:
       body
@@ -62,15 +63,15 @@ runTest "Update from a non-directional button":
 
 runTest "Forced selection":
   let pos = fpvec2(50, 60)
-  let eid = create.with(positioned(pos), Selectable())
+  let eid = create.with(MyCursor(), positioned(pos), Selectable())
   cursor.select(EntitySelected.init(eid, pos).Selected)
   check cursor.selection().to(EntitySelected).entityId == eid
 
 runTest "Animation dimensions impact selection":
   let eids = [
-    createWithAnim.with(positioned(0, 0), Selectable(), newAnimation("img", 500, 10)),
-    createWithAnim.with(positioned(0, 300), Selectable(), newAnimation("img", 30, 10)),
-    createWithAnim.with(positioned(250, 300), Selectable(), newAnimation("img", 30, 10)),
+    createWithAnim.with(MyCursor(), positioned(0, 0), Selectable(), newAnimation("img", 500, 10)),
+    createWithAnim.with(MyCursor(), positioned(0, 300), Selectable(), newAnimation("img", 30, 10)),
+    createWithAnim.with(MyCursor(), positioned(250, 300), Selectable(), newAnimation("img", 30, 10)),
   ]
   cursor.init()
   check cursor.selection().to(EntitySelected).entityId == eids[0]
@@ -79,39 +80,11 @@ runTest "Animation dimensions impact selection":
 
 runTest "Sprite dimensions impact selection":
   let eids = [
-    createWithSprite.with(positioned(0, 0), Selectable(), newSprite("img", 500, 10)),
-    createWithSprite.with(positioned(0, 300), Selectable(), newSprite("img", 30, 10)),
-    createWithSprite.with(positioned(250, 300), Selectable(), newSprite("img", 30, 10)),
+    createWithSprite.with(MyCursor(), positioned(0, 0), Selectable(), newSprite("img", 500, 10)),
+    createWithSprite.with(MyCursor(), positioned(0, 300), Selectable(), newSprite("img", 30, 10)),
+    createWithSprite.with(MyCursor(), positioned(250, 300), Selectable(), newSprite("img", 30, 10)),
   ]
   cursor.init()
   check cursor.selection().to(EntitySelected).entityId == eids[0]
-  cursor.update(kButtonDown)
-  check cursor.selection().to(EntitySelected).entityId == eids[2]
-
-runTest "Ignore hidden animation entities":
-  let hiddenAnim = newAnimation("img", 10, 10)
-  hiddenAnim.visible = false
-
-  let eids = [
-    createWithAnim.with(positioned(0, 0), Selectable(), newAnimation("img", 10, 10)),
-    createWithAnim.with(positioned(0, 100), Selectable(), hiddenAnim),
-    createWithAnim.with(positioned(0, 200), Selectable(), newAnimation("img", 10, 10)),
-  ]
-
-  cursor.init()
-  cursor.update(kButtonDown)
-  check cursor.selection().to(EntitySelected).entityId == eids[2]
-
-runTest "Ignore hidden sprite entities":
-  let hiddenSprite = newSprite("img", 10, 10)
-  hiddenSprite.visible = false
-
-  let eids = [
-    createWithSprite.with(positioned(0, 0), Selectable(), newSprite("img", 10, 10)),
-    createWithSprite.with(positioned(0, 100), Selectable(), hiddenSprite),
-    createWithSprite.with(positioned(0, 200), Selectable(), newSprite("img", 10, 10)),
-  ]
-
-  cursor.init()
   cursor.update(kButtonDown)
   check cursor.selection().to(EntitySelected).entityId == eids[2]
