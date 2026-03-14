4d50003d7f172b26d0cc46064539f833fdac6244
Nycto <James@RoundEights.com>
2026-03-13 18:47:30 -0700

    Add createSingletonSprite

--- src/necsuspd/singleton.nim
+++ src/necsuspd/singleton.nim
@@ -19,3 +19,24 @@ template createsSingletonSpawner*(
       let sheet = assets.newSheet(initialAnimation, zIndex)
       sheet.visible = false
       set(create, (entityTag, sheet, positioned(0, 0), visibility, pausedIn))
+
+template createSingletonSprite*(
+    name: untyped,
+    entityTag: typed,
+    assetBagType: typedesc,
+    loadingState, zIndex: enum,
+    initialAsset: enum,
+    visibility: VisibleState,
+    anchor: AnchorPosition,
+    absolutePos: bool,
+) =
+  ## Creates a system for spawning a singleton entity backed by a static Sprite
+  proc `name`*(
+      task: Bundle[LoadTasks],
+      assets: Shared[assetBagType],
+      create: Spawn[(typeof(entityTag), Sprite, Positioned, VisibleState)],
+  ) {.active(loadingState).} =
+    task.execTask($typeof(entityTag), typeof(entityTag), 0):
+      let sprite = assets.newAssetSprite(initialAsset, anchor, zIndex, absolutePos)
+      sprite.visible = false
+      set(create, (entityTag, sprite, positioned(0, 0), visibility))
